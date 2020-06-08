9da7320054de057da69f03864a10d21a27f15ec2
Betuca <betuca69@gmail.com>
2020-06-08 09:10:07 -0300

    Internationalization POC

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useTranslation } from "react-i18next";
+import './i18n';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+
+    const { t, i18n } = useTranslation();
+    // i18next.changeLanguage('jap', (err, t) => {
+    //     if (err) return console.log('something went wrong loading', err);
+    //     console.log(t('title')); // -> same as i18next.t
+    // });
+    return (
+        <div>
+            <h1>{t("title")}</h1>
+            <img src="./japan.png"></img>
+            <p>{t("main text")}</p>
+            <button onClick={() => i18n.changeLanguage('jap')}>de</button>
+            <button onClick={() => i18n.changeLanguage('en')}>en</button>
+        </div>
+    );
 }
 
 export default App;
+
+
--- src/i18n.js
+++ src/i18n.js
@@ -1,8 +1,5 @@
-import React from "react";
-import ReactDOM from "react-dom";
-// import i18n from './i18n';
 import i18n from "i18next";
-import { useTranslation, initReactI18next } from "react-i18next";
+import { initReactI18next } from "react-i18next";
 
 i18n
     .use(initReactI18next) // passes i18n down to react-i18next
@@ -34,23 +31,23 @@ i18n
         }
     });
 
-const App = () => {
-    const { t } = useTranslation();
-    console.log('i18n: ', i18n);
+export default i18n;
 
-    return (
-        <div>
-            <h1>{t("title")}</h1>
-            <img src="./japan.png" alt={t("title")}></img>
-            <p>{t("main text")}</p>
-            <button onClick={() => i18n.changeLanguage('en')}>en</button>
-            <button onClick={() => i18n.changeLanguage('jap')}>jap</button>
-        </div>
-    );
-}
 
-// append app to dom
-ReactDOM.render(
-    <App />,
-    document.getElementById("root")
-);
+
+// const resources = {
+//     en: {
+//         translations: {
+//             "title": "Japan",
+//             "main text":
+//                 "is an island country in East Asia. Located in the northwest Pacific Ocean, it borders the Sea of Japan to the west, and extends from the Sea of Okhotsk in the north to the East China Sea and Taiwan in the south. Japan is part of the Pacific Ring of Fire and comprises an archipelago of 6,852 islands covering 377,975 square kilometers (145,937 sq mi); its five main islands, from north to south, are Hokkaido, Honshu, Shikoku, Kyushu, and Okinawa. Tokyo is the country's capital and largest city; other major cities include Osaka and Nagoya."
+//         }
+//     },
+//     jap: {
+//         translations: {
+//             "title": "日本",
+//             "main text":
+//                 "東アジアの島国です。北西太平洋に位置し、日本海と西に接し、北はオホーツク海から南は東シナ海、台湾へと伸びています。日本はパシフィックリングオブファイアの一部であり、377,975平方キロメートル（145,937平方マイル）をカバーする6,852の島からなる群島で構成されています。北から南に5つの主要な島は、北海道、本州、四国、九州、沖縄です。東京は国の首都であり最大の都市です。他の主要都市には、大阪と名古屋が含まれます。"
+//         }
+//     }
+// };
